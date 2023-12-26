17df4ecfbc64f2892574608b66594b8d739bb2e2
Bachtuoc <davidjohnbachtuoc@gmail.com>
2023-12-26 19:27:44 +0700

    + get texture for direction sign and taxi

--- src/magicTiles3/assetSelector.js
+++ src/magicTiles3/assetSelector.js
@@ -1,4 +1,6 @@
 import songData from "../../assets/jsons/songData.json";
+import { Util } from "../helpers/utils";
+import { CollisionTag } from "../physics/aabb/collisionTag";
 import { Tween } from "../systems/tween/tween";
 export class AssetSelector {
  
@@ -6,4 +8,20 @@ export class AssetSelector {
     return PIXI.Texture.from("Theme1_Background");
   }
   
+  static getVehicleRunTextures() {
+    return Util.getTexturesContain(`taxi_idle`);
+  }
+
+  static getDirectionSignTextureByTag(tag) {
+    switch (tag) {
+      case CollisionTag.TurnLeftSign:
+        return PIXI.Texture.from("spr_turnLeft");
+      case CollisionTag.TurnRightSign:
+        return PIXI.Texture.from("spr_turnRight");
+      case CollisionTag.TurnBackSign:
+        return PIXI.Texture.from("spr_turnBack");
+      default:
+        break;
+    }
+  }
 }
